ed43b05d131118e13dace535bca4ff9918499016
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-02-26 11:45:19 +0100

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryDiv = document.querySelector(".gallery");
+
+galleryItems.forEach((i) => {
+  const galleryItem = `<div class="gallery__item">
+        <a class = "gallery__link" href = "${i.original}">
+        <img class="gallery__image" src="${i.preview}" data-source="${i.original}" alt="${i.description}"/>
+        </a></div>`;
+  galleryDiv.insertAdjacentHTML("beforeend", galleryItem);
+});
 
 console.log(galleryItems);

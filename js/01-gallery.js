9109aba9c54c1552f674944ae7cb25dc41cf6adc
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-02-26 12:42:23 +0100

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,10 +5,16 @@ const galleryDiv = document.querySelector(".gallery");
 
 galleryItems.forEach((i) => {
   const galleryItem = `<div class="gallery__item">
-        <a class = "gallery__link" href = "${i.original}">
-        <img class="gallery__image" src="${i.preview}" data-source="${i.original}" alt="${i.description}"/>
-        </a></div>`;
+    <a class = "gallery__link" href = "${i.original}">
+    <img class="gallery__image" src="${i.preview}" data-source="${i.original}" alt="${i.description}"/>
+    </a></div>`;
   galleryDiv.insertAdjacentHTML("beforeend", galleryItem);
 });
 
 console.log(galleryItems);
+
+document.querySelector(".gallery__link").onclick = (event) => {
+  event.preventDefault();
+  bascLightbox.create(`<img width="1400" height="900" src="${i.original}">`);
+  bascLightbox.show();
+};
